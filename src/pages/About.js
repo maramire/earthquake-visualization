bf7420e242131ea336c86a8b260f41b88d18961f
maramire <matias.ramirezg@sansano.usm.cl>
2021-09-20 19:29:52 -0300

    About page edited

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,16 @@
+import styles from "./About.module.css";
+
 function About() {
-  return <div>about</div>;
+  return (
+    <div className={styles.main}>
+      <h1>About</h1>
+      <p>
+        Earthquake viewer is a basic (non-responsive yet) website created with
+        frontend framework React for learning purposes. It uses the API provided
+        by USGS to track the most recent events ocurred in a certain place.
+      </p>
+    </div>
+  );
 }
 
 export default About;
