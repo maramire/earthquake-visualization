ca65901b39e5bed09f684037d12edb74b1dbe50f
maramire <matias.ramirezg@sansano.usm.cl>
2021-09-23 14:17:53 -0300

    Added HTTP requests using fetch api

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -1,45 +1,86 @@
 import { useParams } from "react-router-dom";
 import { events } from "./dummyData";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import styles from "./EventDetail.module.css";
 import Card from "../components/Card";
 import Map from "../components/Map";
 
 function EventDetail() {
-  // const params = useParams();
-  const event = events[0];
-  const eventDate = new Date(event.properties.time).toLocaleString();
+  // destructuring params
+  const { eventId } = useParams();
+  const [event, setEvent] = useState({});
+  const [dataIsLoaded, setDataIsLoaded] = useState(false);
+
+  // send request only the first time the component was rendered
+  useEffect(() => {
+    const url = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventid=${eventId}`;
+    console.log("fetching single event");
+    fetch(url)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setEvent(result);
+          setDataIsLoaded(true);
+        },
+        (error) => {}
+      );
+  }, [eventId]);
+
+  function getEventDateFormatted(date) {
+    return new Date(date).toLocaleString();
+  }
 
   return (
     <Fragment>
-      <div className={styles.main}>
-        <h1>Event Detail: {event.properties.title}</h1>
-        <p>Event Date: {eventDate}</p>
-      </div>
-      <div className={styles["section-one"]}>
-        <Card title="Detalle de evento">
-          <ul className={styles["event-description"]}>
-            <li>
-              Magnitude: {`${event.properties.mag} ${event.properties.magType}`}
-            </li>
-            <li>Depth: {`${event.geometry.coordinates[2]} km`}</li>
-            <li>
-              Significance: {event.properties.sig}
-              <span className={styles.significance}>/1000</span>
-            </li>
-          </ul>
-        </Card>
-      </div>
-      <div className={styles["section-two"]}>
-        <Card title="Mapa de evento">
-          <Map
-            events={[event]}
-            zoom={6}
-            lat={event.geometry.coordinates[1]}
-            lng={event.geometry.coordinates[0]}
-          />
-        </Card>
-      </div>
+      {dataIsLoaded && (
+        <Fragment>
+          <div className={styles.main}>
+            <h1>Event Detail: {event.properties.title}</h1>
+            <p>Event Date: {getEventDateFormatted(event.properties.time)}</p>
+          </div>
+          <div className={styles["section-one"]}>
+            <Card title="Detalle de evento">
+              <ul className={styles["event-description"]}>
+                <li>
+                  Magnitude:{" "}
+                  <span
+                    className={styles.value}
+                  >{`${event.properties.mag} ${event.properties.magType}`}</span>
+                </li>
+                <li>
+                  Depth:{" "}
+                  <span
+                    className={styles.value}
+                  >{`${event.geometry.coordinates[2]} km`}</span>
+                </li>
+                <li>
+                  Significance:{" "}
+                  <span className={styles.value}>
+                    {event.properties.sig}
+                    <span className={styles.significance}>/1000</span>
+                  </span>
+                </li>
+              </ul>
+            </Card>
+          </div>
+          <div className={styles["section-two"]}>
+            <Card title="Event Map">
+              <Map
+                events={[event]}
+                zoom={6.5}
+                lat={event.geometry.coordinates[1]}
+                lng={event.geometry.coordinates[0]}
+              />
+            </Card>
+          </div>
+        </Fragment>
+      )}
+      {!dataIsLoaded && (
+        <div className={styles.main}>
+          <h1>Loading data...</h1>
+        </div>
+      )}
     </Fragment>
   );
 }
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,12 +1,46 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
 import styles from "./Events.module.css";
 import Card from "../components/Card";
 import Map from "../components/Map";
 import EventsList from "../components/EventsList";
-import { events } from "./dummyData";
+// import { events } from "./dummyData";
 
 function Events() {
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const isEventsEmpty = events.length <= 0;
+
+  // send request only the first time the component was rendered
+  useEffect(() => {
+    const url = new URL("https://earthquake.usgs.gov/fdsnws/event/1/query");
+    const params = {
+      format: "geojson",
+      starttime: "2021-09-23",
+      endtime: "2021-09-24",
+      minlatitude: "-75.05689251672965",
+      maxlatitude: "-14.288953187818608",
+      minlongitude: "-95.70302236800661",
+      maxlongitude: "-63.227436430506614",
+      orderby: "time",
+    };
+    // create the url with params
+    Object.keys(params).forEach((key) =>
+      url.searchParams.append(key, params[key])
+    );
+    console.log("fetching all events");
+    fetch(url)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setIsLoading(false);
+          setEvents(result.features);
+        },
+        (error) => {
+          setIsLoading(false);
+        }
+      );
+  }, []);
 
   return (
     <Fragment>
@@ -22,12 +56,22 @@ function Events() {
       </div>
       <div className={styles["section-two"]}>
         <Card title="Events Map" footer="Location: Chile">
-          <Map
-            events={events}
-            zoom={2.5}
-            lat={-36.33325814457118}
-            lng={-71.39361021304366}
-          />
+          {isEventsEmpty && (
+            <Map
+              events={[]}
+              zoom={2.5}
+              lat={-36.33325814457118}
+              lng={-71.39361021304366}
+            />
+          )}
+          {!isEventsEmpty && (
+            <Map
+              events={events}
+              zoom={2.5}
+              lat={-36.33325814457118}
+              lng={-71.39361021304366}
+            />
+          )}
         </Card>
       </div>
     </Fragment>
