29dcff15cea912387182742a46e75cb242459b8e
maramire <matias.ramirezg@sansano.usm.cl>
2022-09-10 03:58:25 -0300

    add mobile map styles

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,9 +2,16 @@ import ReactMapGL from "react-map-gl";
 import { useMemo, useRef, useContext } from "react";
 import MapMarker from "./MapMarker";
 import MapContext from "../store/map-context";
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import { useTheme } from '@mui/material/styles';
 
 function Map(props) {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.between('xs','md'));
+
+  const mapStyle = { height: "70vh", width: "50vw" }
+  const mobileMapStyle = { height: "70vh", width: "80vw" }
+
   const mapRef = useRef();
   const mapContext = useContext(MapContext);
   // callback passed to MapMarker when marker is clicked
@@ -33,7 +40,7 @@ function Map(props) {
     mapContext.setBounds(coords);
   };
   return (
-    <Box >
+    <Box>
       <ReactMapGL
         initialViewState={{
           latitude: -36.33325814457118,
@@ -41,9 +48,9 @@ function Map(props) {
           zoom: 2.5,
         }}
         ref={mapRef}
-        onMouseUp={touchEndHandler}
         onWheel={touchEndHandler}
-        style={{width: 600, height: 500}}
+        onDragEnd={touchEndHandler}
+        style={isMobile ? mobileMapStyle : mapStyle}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
